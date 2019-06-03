9d9b84fac52a872a9563c9f273a4e99b1bdc6913
dimlightray <dimlightray@gmail.com>
2019-06-03 19:49:13 +0900

    deviceside development

---
+++ DeviceSide/BullyingDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullyingDetection
+//
+//  Created by 신유정 on 28/04/2019.
+//  Copyright © 2019 Dung Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
